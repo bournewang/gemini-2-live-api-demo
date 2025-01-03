4864c0a2b33c898ae1f3a43a7af4aa3a93aa790f
Bourne <xiaopei0206@icloud.com>
2025-01-03 17:07:25 +0800

    fix gemini key

--- js/config/config.js
+++ js/config/config.js
@@ -1,6 +1,6 @@
 export const CONFIG = {
     API: {
-        KEY: process.env.GEMINI_KEY,
+        KEY: GEMINI_KEY,
         BASE_URL: 'wss://generativelanguage.googleapis.com/ws',
         VERSION: 'v1alpha',
         MODEL_NAME: 'models/gemini-2.0-flash-exp'
